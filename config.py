355db570faec3f2818e6630463d8b3475290972d
justfantasy <27506954@qq.com>
2016-06-29 10:57:41 +0800

    'second'

--- config.py
+++ config.py
@@ -4,17 +4,23 @@
 __author__ = 'JustFantasy'
 
 # 基础路径
-local_basepath = u'F:/code/测试/'
-target_basepath = '/data/tmp/'
+local_basepath = u'F:/code/'     # 本地基础路径
+target_basepath = '/data/tmp/'       # 远程基础路径
+target_bak_basepath = '/data/bak/'     # 远程备份路径
+
+# 没有项目名称源地址
+exclude_path = (
+    'at.vue.cn'
+)
 
 # 本地项目对应的绝对路径
 local_path_map = {
-    'www.vue.com': local_basepath + u'测试/'
+    'www.vue.com': local_basepath + u'测试/',
 }
 
 # 远程项目对应的绝对路径
 target_path_map = {
-    'www.vue.com': target_basepath
+    'www.vue.com': target_basepath,
 }
 
 
--- fabfile.py
+++ fabfile.py
@@ -9,6 +9,10 @@ fab -f s1.py go
 fab hello:name=age,value=20 或者 fab hello:age,20 带参数的使用方法
 
 examples: http://wklken.me/posts/2013/03/25/python-tool-fabric.html
+
+todo:   解决没有目录的情况
+        增加备份和回滚
+
 '''
 __author__ = 'JustFantasy'
 
@@ -16,16 +20,18 @@ from fabric.api import *
 from fabric.colors import *
 from fabric.context_managers import *
 from fabric.contrib.console import confirm
+from fabric.contrib import files
+#from fabric.contrib import project
 import config, os, os.path
+from datetime import *
 
 # 配置连接服务器的参数
 env.user = 'root'
-env.hosts = ['10.0.8.88']
-env.password = 'vue@test_0000'
+env.hosts = ['192.168.1.64']
+env.password = '123456'
 
 # 代码上传
-def code_upload(path):
-    print yellow(u'正在上传代码到服务器...')
+def code_upload(path, remoteBakName):
     with settings(warn_only=True):
 
         # 格式转化
@@ -37,31 +43,44 @@ def code_upload(path):
         except KeyError:
             localAbsPath = config.local_basepath + path
 
+        # 如果路径中没有项目名称，则去掉项目名称
+        if pathParams[0] in config.exclude_path:
+            localAbsPath = localAbsPath.replace(pathParams[0] + '/', '')
+
+        # 判断本地文件是否存在
+        if not os.path.exists(localAbsPath) and not confirm(localAbsPath + ' is not exists, Continue?'):
+            abort('Aborting file put task!')
+
         # 获取远程绝对路径
         try:
             remoteAbsPath = config.target_path_map[pathParams[0]] + path
         except KeyError:
-            remoteAbsPath = config.target_path_map + path
+            remoteAbsPath = config.target_basepath + path
 
+        # 文件备份操作
+        if files.exists(remoteAbsPath):
+            run('tar -cPvf ' + remoteBakName + ' ' + remoteAbsPath)
 
         # 判断是否是文件，如果是目录，则遍历出所有的文件
         if os.path.isfile(localAbsPath):
             upload(localAbsPath, remoteAbsPath)
         else:
-            #三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
+            # 三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
             for parent, dirnames, filenames in os.walk(localAbsPath):
                 for filename in filenames:
                     tmpAbsPath = os.path.join(parent, filename).replace('\\', '/')
                     targetPath = tmpAbsPath.replace(localAbsPath, remoteAbsPath)
                     upload(tmpAbsPath, targetPath)
 
+            # 如果是linux服务器，可以直接使用这个方法进行代码更新
+            # project.rsync_project(remoteAbsPath, localAbsPath)
+
 # 文件上传操作
 def upload(source, targetPath):
 
-    # 首先判断目录是否存在，不存在则创建
+    # 若远程目录不存在则创建
     targetDir = os.path.dirname(targetPath)
-
-    if not os.path.exists(targetDir):
+    if not files.exists(targetDir):
         run('mkdir -p ' + targetDir)
 
     #上传操作
@@ -69,12 +88,26 @@ def upload(source, targetPath):
     if result.failed and not confirm('put file failed, Continue?'):
         abort('Aborting file put task!')
 
-
+# @desc: 代码上传
+# @exec: 调用方式，fab go
 @task
 def go():
+    print yellow(u'正在上传代码到服务器...')
+
+    remoteBakName = config.target_bak_basepath.rstrip('/') + '/' + datetime.now().strftime('%Y%m%d%H%M') + '.tar'
+    print yellow(u'备份文件路径：' + remoteBakName)
+
     with open('source.txt') as f:
         for line in f.readlines():
-            code_upload(line.strip())
+            code_upload(line.strip(), remoteBakName)
+
+# @desc: 代码回滚，v表示tar包的名称
+# @exec: 调用方式，fab back:v=201606291015
+@task()
+def back(v):
+    print yellow(u'将代码回退到版本：' + v)
+    with cd(config.target_bak_basepath):
+        run('tar -xPvf ' + v + '.tar')
 
 
 
